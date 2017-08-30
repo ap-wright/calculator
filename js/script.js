c7736dd28b28b54f0557fa6734a8b8ce933c568f
April Wright <jham13@yahoo.com>
2017-08-30 15:32:31 +1000

    Add celcius and farhenhiet calculations.

--- js/script.js
+++ js/script.js
@@ -37,3 +37,28 @@ var number2 = parseInt(prompt("Enter another number:"));
 var result = divide(number1, number2);
 
 alert(result);
+
+//Celcius to Farhenheit //
+
+var celcius = parseInt(prompt("Enter the temperature in Celcius."));
+var result = (celcius * 1.8) + 32;
+
+alert("Your answer is: " + result);
+
+var farhenheit = parseInt(prompt("Enter the temperature in Farhenheit."));
+var result = (farhenheit - 32) * 5/9;
+
+alert("Your answer is " + result);
+
+
+//This doesn't work //
+var bmi = function (height,weight) {
+	return height * height / weight;
+}
+
+var height = parseInt(prompt("Enter your height in m:"));
+var weight = parseInt(prompt("Enter your weight in kg:"));
+var result = muliply(height, height) + divide (weight);
+
+alert(result);
+//end of code that doesn't work. Couldn't figure out how to multipy and divide at same time //
